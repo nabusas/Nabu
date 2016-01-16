fdf9b8056366eb6debfa57772fedd9f7797cb32d
Carlos Alberto Garcia Cobo <carlosgc4@gmail.com>
2016-01-16 13:33:51 -0500

    1.4

--- Core/Pages/nabu.php
+++ Core/Pages/nabu.php
@@ -34,7 +34,7 @@ THE SOFTWARE.
     
 	session_start();
     
-    $objUtilities = new Utilities('localhost','root','n4b62015','nabu_vanilla');
+    $objUtilities = new Utilities('localhost','nabu','n4b62015','nabu_vanilla');
     $objTemplate =new TemplatePage($objUtilities);
 
     if($_GET['p'] == 'login'){
