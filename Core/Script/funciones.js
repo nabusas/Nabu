1922edaf5e519cd90cb9e80cb54a5a60b6bfae2d
Carlos Alberto Garcia Cobo <carlosgc4@gmail.com>
2016-01-28 08:47:31 -0500

    1.23 :koala:

--- Core/Script/funciones.js
+++ Core/Script/funciones.js
@@ -1,5 +1,9 @@
 var buscarCliente = function (doc) {
+    
     "use strict";
     var ruta = "nabu.php?p=nb_facturacion_pg&accion=s&nb_fact_3_fld=" + doc;
-    document.location = ruta; 
+    
+    if (!isNaN(doc)) {
+        document.location = ruta;
+    }
 };
