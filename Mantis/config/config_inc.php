4154689a544394a30d5d68b4e55847f8634856f5
Carlos Alberto Garcia Cobo <carlosgc4@gmail.com>
2017-03-15 14:20:24 -0500

    3.22

--- Mantis/config/config_inc.php
+++ Mantis/config/config_inc.php
@@ -5,8 +5,21 @@ $g_database_name          = 'nabutraq';
 $g_db_username            = 'nabu';
 $g_db_password            = '6492496';
 
-$g_default_timezone       = 'Europe/Berlin';
+
 
 $g_crypto_master_salt     = 'CqI/5fm9XJca3RJJvShvBKTzZ5X7lq1f8+GJE90gMsw=';
+
+//CAGC
+
+//Inactivar Signup for a new account
+$g_allow_signup = OFF;
+
+//Idioma
+$g_default_language = 'spanish';
+
+//¿Olvidó su contraseña?
+$g_send_reset_password = OFF;
+
+$g_default_timezone       = 'America/Bogota';
 
 ?>
