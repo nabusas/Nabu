6219d1b4647bfefafb5212bffbc68f75a99560ff
nabudeveloper <30754821+nabudeveloper@users.noreply.github.com>
2017-08-15 18:18:52 +0200

    Cambio en tamaño de celdas
    Cambio en tamaño de celdas

--- Core/Reports/cobrosPendientes.php
+++ Core/Reports/cobrosPendientes.php
@@ -68,43 +68,45 @@ function schemaReport($pdf,$tamanoFuenteForm,$cabecera,$detalle,$totales,$fecha_
         
         $pdf->SetFont('helvetica', 'B', $tamanoFuenteForm+1); 
         $pdf->Cell(25,$w,'Fecha de cobro', $borde,0,'C');
-        $pdf->Cell(25,$w,'Referencia factura',$borde,0,'C');
+        $pdf->Cell(27,$w,'Referencia factura',$borde,0,'C');
         $pdf->Cell(25,$w,'Cliente',$borde,0,'C');
-        $pdf->Cell(20,$w,'Barrio',$borde,0,'C');
+        $pdf->Cell(25,$w,'Barrio',$borde,0,'C');
         $pdf->Cell(25,$w,'Direccion',$borde,0,'C');
     	$pdf->Cell(25,$w,'Telefono',$borde,0,'C');
-    	$pdf->Cell(27,$w,'Valor factura',$borde,0,'C');
-    	$pdf->Cell(27,$w,'Valor abonos',$borde,0,'C');
-    	$pdf->Cell(27,$w,'Valor descuento',$borde,0,'C');	
-    	$pdf->Cell(27,$w,'Saldo pendiente',$borde,0,'C');
-	//$pdf->Cell(25,$w,'Abono realizado',$borde,0,'C');
-	//$pdf->Cell(25,$w,'Fecha proximo pago',$borde,0,'C');	
+    	$pdf->Cell(28,$w,'Valor factura',$borde,0,'C');
+    	$pdf->Cell(28,$w,'Valor abonos',$borde,0,'C');
+    	$pdf->Cell(28,$w,'Valor descuento',$borde,0,'C');	
+    	$pdf->Cell(28,$w,'Saldo pendiente',$borde,0,'C');
+    	//$pdf->Cell(25,$w,'Abono realizado',$borde,0,'C');
+    	//$pdf->Cell(25,$w,'Fecha proximo pago',$borde,0,'C');	
         $pdf->Ln(5);
 
         $pdf->SetFont('helvetica', 'N', $tamanoFuenteForm);  
         
-        for ($i=0; $i<sizeof($detalle); $i++){
+        for ($i=0; $i<sizeof($detalle); $i++)
+        {
             $pdf->Cell(25,$w,$detalle[$i]['fechadecobro'], $borde,0,'C');
-            $pdf->Cell(25,$w,$detalle[$i]['factura'],$borde,0,'C');
+            $pdf->Cell(27,$w,$detalle[$i]['factura'],$borde,0,'C');
             $pdf->Cell(25,$w,$detalle[$i]['nombre'],$borde,0,'C');
-            $pdf->Cell(20,$w,$detalle[$i]['barrio'],$borde,0,'C');
+            $pdf->Cell(25,$w,$detalle[$i]['barrio'],$borde,0,'C');
             $pdf->Cell(25,$w,$detalle[$i]['direccion'],$borde,0,'C');
     	    $pdf->Cell(25,$w,$detalle[$i]['telefono'],$borde,0,'C');
-    	    $pdf->Cell(27,$w,$detalle[$i]['valorfactura'],$borde,0,'C');
-    	    $pdf->Cell(27,$w,$detalle[$i]['valorabonos'],$borde,0,'C');
-    	    $pdf->Cell(27,$w,$detalle[$i]['valordescuentos'],$borde,0,'C');
-    	    $pdf->Cell(27,$w,$detalle[$i]['saldopendiente'],$borde,0,'C');
+    	    $pdf->Cell(28,$w,$detalle[$i]['valorfactura'],$borde,0,'C');
+    	    $pdf->Cell(28,$w,$detalle[$i]['valorabonos'],$borde,0,'C');
+    	    $pdf->Cell(28,$w,$detalle[$i]['valordescuentos'],$borde,0,'C');
+    	    $pdf->Cell(28,$w,$detalle[$i]['saldopendiente'],$borde,0,'C');
     	    //$pdf->Cell(25,$w,'',$borde,0,'C');
     	    //$pdf->Cell(25,$w,'',$borde,0,'C');
             $pdf->Ln(5);
         }
-	$pdf->Ln(5);
-	$pdf->SetFont('helvetica', 'B', $tamanoFuenteForm+1); 
+
+    	$pdf->Ln(5);
+    	$pdf->SetFont('helvetica', 'B', $tamanoFuenteForm+1); 
         $pdf->Cell(50,$w,"Total facturas a cobrar:",$borde,0, 'L');
         $pdf->SetFont('helvetica', 'N', $tamanoFuenteForm);   
         $pdf->Cell(99,$w,$cabecera['totalfacturas'],$borde,0, 'L');
-	$pdf->Ln(5);
-	$pdf->SetFont('helvetica', 'B', $tamanoFuenteForm+1); 
+    	$pdf->Ln(5);
+    	$pdf->SetFont('helvetica', 'B', $tamanoFuenteForm+1); 
         $pdf->Cell(50,$w,"Total saldo a cobrar:",$borde,0, 'L');
         $pdf->SetFont('helvetica', 'N', $tamanoFuenteForm);   
         $pdf->Cell(99,$w,$cabecera['totalsaldo'],$borde,0, 'L');
