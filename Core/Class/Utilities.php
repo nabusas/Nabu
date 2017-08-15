ad4e28c3174aa3a17f1e0320fe17bff877e6f961
Carlos Alberto Garcia Cobo <carlosgc4@gmail.com>
2017-08-15 10:00:54 -0500

    3.111

--- Core/Class/Utilities.php
+++ Core/Class/Utilities.php
@@ -766,7 +766,7 @@ class Utilities
         
         if ( $_GET['p'] = 'nb_relacionfactura_pg' and  $_POST[$field['nb_estado_cartera_fld']] <> '3' ){
             $factura =$_POST[$field['nb_factura_fld']];
-            $pagelink  ='nb_cartera_pg?nb_referencia_fld='.$factura;
+            $pagelink  ='nb_cartera_pg&nb_referencia_fld='.$factura;
         }
         else{
             $pagelink=$nabuEvent->getpagelink($_GET['p']);
