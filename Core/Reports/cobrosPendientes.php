ae162faa498df0864689fd29743480b25ef81322
nabudeveloper <30754821+nabudeveloper@users.noreply.github.com>
2017-08-15 03:13:39 +0200

    Cambio en tamaño de letra
    Cambio en tamaño de letra

--- Core/Reports/cobrosPendientes.php
+++ Core/Reports/cobrosPendientes.php
@@ -133,7 +133,7 @@ function schemaReport($pdf,$tamanoFuenteForm,$cabecera,$detalle,$totales,$fecha_
 
     $pdf=$objReport->setupForm();
 
-    schemaReport($pdf,5,$cabecera,$detalle,NULL, $fecha_desde, $fecha_hasta,$zonanombre);
+    schemaReport($pdf,7,$cabecera,$detalle,NULL, $fecha_desde, $fecha_hasta,$zonanombre);
 
     $objReport->exportarPdf($pdf,$id);
 
