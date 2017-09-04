1d167fb3269e6dd719b36e829e0d8ebfee69d74a
Carlos Alberto Garcia Cobo <carlosgc4@gmail.com>
2017-09-03 23:50:13 -0500

    3.177

--- Core/Reports/consolidadoCartera.php
+++ Core/Reports/consolidadoCartera.php
@@ -43,31 +43,165 @@
     $database = $objUtilities->database;
 
     $sqlZonas=' ';
-    if (isset($zona) and $zona <> '')
+    if (isset($zona) and $zona <> ''){
         $sqlZonas= "where ID_ZONA =".$zona;
+    }
+    
 
-    $rangoFecha =$sql."BETWEEN STR_TO_DATE('".$fecha_cartera_desde."','%d/%m/%Y') AND STR_TO_DATE('".$fecha_cartera_hasta."','%d/%m/%Y') ";
+    $sql="
+    SELECT reporte.ID_ZONA AS ZONA,
+       upper(reporte.nb_nombre_fld) AS 'NOMBRE ZONA',
+   (SELECT count(a.referencia)
+   FROM nb_ventas_grid_vw a
+   JOIN nb_zonasFacturas_vw zonas on (zonas.factura=  upper(a.referencia)  
+   )
+   WHERE replace(replace(saldo,'$',''),',','') > 0
+     and estado = 'ACTIVO'
+     and zonas.nb_id_fld = reporte.ID_ZONA
+     and IFNULL(EstadoCartera,'X')  <> 'Castigada'
+     AND date(fechaingreso) <= STR_TO_DATE('".$fecha_cartera_desde."','%d/%m/%Y') )AS 'FACTURAS INICIALES',
+   (SELECT ifnull(sum(replace(replace(saldo,'$',''),',','')),0)
+   FROM nb_ventas_grid_vw a
+   JOIN nb_zonasFacturas_vw zonas on (zonas.factura=  upper(a.referencia)  
+   )
+   WHERE replace(replace(saldo,'$',''),',','') > 0
+     and estado = 'ACTIVO'
+     and zonas.nb_id_fld = reporte.ID_ZONA
+     and IFNULL(EstadoCartera,'X')  <> 'Castigada'
+     AND date(fechaingreso) <= STR_TO_DATE('".$fecha_cartera_desde."','%d/%m/%Y') ) AS 'SALDO INICIAL',
+       (select
+	  count(a.nb_id_fld)
+      FROM nb_relacionfactura_tbl A
+      JOIN nb_zonasFacturas_vw zonas ON (zonas.factura= upper(A.NB_FACTURA_FLD))
+      WHERE zonas.nb_id_fld = reporte.ID_ZONA
+      AND (STR_TO_DATE(A.nb_fecha_entrega_fld, '%d/%m/%Y') BETWEEN STR_TO_DATE('".$fecha_cartera_desde."','%d/%m/%Y') and STR_TO_DATE('".$fecha_cartera_hasta."','%d/%m/%Y'))
+      AND A.nb_estado_fld = 0) AS 'FACTURAS ENTREGADAS',
+      (
+	  SELECT count(cartera.nb_id_fld)
+      FROM  nb_cartera_tbl cartera
+      JOIN nb_zonasFacturas_vw zonas ON (zonas.factura= SUBSTRING(upper(cartera.nb_referencia_fld),2,length(cartera.nb_referencia_fld)))
+      WHERE cartera.nb_concepto_fld=1
+      AND cartera.nb_id_fld = ( select max(cartera2.nb_id_fld) from nb_cartera_tbl cartera2 where cartera.nb_referencia_fld= cartera2.nb_referencia_fld )
+      AND SUBSTRING(upper(cartera.nb_referencia_fld),1,1) ='V'
+      AND replace(replace(cartera.nb_nuevo_saldo_fld,'$',''),',','') > 0
+       AND zonas.nb_id_fld = reporte.ID_ZONA
+      AND (STR_TO_DATE(cartera.nb_fecha_ingreso_concepto_fld, '%d/%m/%Y') BETWEEN STR_TO_DATE('".$fecha_cartera_desde."','%d/%m/%Y') and STR_TO_DATE('".$fecha_cartera_hasta."','%d/%m/%Y'))
+      AND cartera.nb_estado_fld = 0
+      ) AS 'FACTURAS ABONADAS',
+     (
+       SELECT count(cartera.nb_id_fld)
+      FROM  nb_cartera_tbl cartera
+      JOIN nb_zonasFacturas_vw zonas ON (zonas.factura= SUBSTRING(upper(cartera.nb_referencia_fld),2,length(cartera.nb_referencia_fld)))
+      WHERE cartera.nb_concepto_fld=1
+      AND cartera.nb_id_fld = ( select max(cartera2.nb_id_fld) from nb_cartera_tbl cartera2 where cartera.nb_referencia_fld= cartera2.nb_referencia_fld )
+      AND SUBSTRING(upper(cartera.nb_referencia_fld),1,1) ='V'
+      AND replace(replace(cartera.nb_nuevo_saldo_fld,'$',''),',','') = 0
+       AND zonas.nb_id_fld = reporte.ID_ZONA
+      AND (STR_TO_DATE(cartera.nb_fecha_ingreso_concepto_fld, '%d/%m/%Y') BETWEEN STR_TO_DATE('".$fecha_cartera_desde."','%d/%m/%Y') and STR_TO_DATE('".$fecha_cartera_hasta."','%d/%m/%Y'))
+      AND cartera.nb_estado_fld = 0
+      
+      ) AS 'FACTURAS CANCELADAS',
+         (
+      SELECT ifnull(sum(replace(replace(cartera.nb_valor_fld,'$',''),',','')),0)
+      FROM  nb_cartera_tbl cartera
+      JOIN nb_zonasFacturas_vw zonas ON (zonas.factura= SUBSTRING(upper(cartera.nb_referencia_fld),2,length(cartera.nb_referencia_fld)))
+      WHERE cartera.nb_concepto_fld=1
+      AND SUBSTRING(upper(cartera.nb_referencia_fld),1,1) ='V'
+       AND zonas.nb_id_fld = reporte.ID_ZONA
+      AND (STR_TO_DATE(cartera.nb_fecha_ingreso_concepto_fld, '%d/%m/%Y') BETWEEN STR_TO_DATE('".$fecha_cartera_desde."','%d/%m/%Y') and STR_TO_DATE('".$fecha_cartera_hasta."','%d/%m/%Y'))
+      AND cartera.nb_estado_fld = 0
+      
+      ) AS 'RECAUDO',
+       (
+            select count(1) 
+            from nb_ventas_grid_vw venta
+            JOIN nb_zonasFacturas_vw zonas ON (zonas.factura= venta.referencia)
+            where venta.estado = 'ACTIVO'
+            and  venta.formapago <> 'CONTADO'
+            AND zonas.nb_id_fld = reporte.ID_ZONA
+            AND date(fechaingreso) BETWEEN STR_TO_DATE('".$fecha_cartera_desde."','%d/%m/%Y') and STR_TO_DATE('".$fecha_cartera_hasta."','%d/%m/%Y')
 
-    $sql="select ID_ZONA as ZONA,";
-    $sql =$sql."upper(a.nb_nombre_fld) AS 'NOMBRE ZONA',";
-    $sql =$sql."(select count(1) from nb_ventas_grid_vw where EstadoCartera <> '' and date(fechaingreso) $rangoFecha )AS 'FACTURAS INICIALES',";
-    $sql =$sql."1  AS 'SALDO INICIAL',";
-    $sql =$sql."1  AS 'FACTURAS ENTREGADAS',";
-    $sql =$sql."1  AS 'FACTURAS ABONADAS',";
-    $sql =$sql."1  AS 'FACTURAS CANCELADAS',";
-    $sql =$sql."1  AS 'RECAUDO',";
-    $sql =$sql."1  AS 'FACTURAS VENDIDAS',";
-    $sql =$sql."1  AS 'NUEVA CARTERA',";
-    $sql =$sql."1  AS 'FACTURAS CASTIGADA',";
-    $sql =$sql."1  AS 'MONTO CASTIGADO',";
-    $sql =$sql."1  AS 'TRASLADOS IN',";
-    $sql =$sql."1  AS 'MONTO TRASLADO IN',";
-    $sql =$sql."1  AS 'TRASLADOS OUT',";
-    $sql =$sql."1  AS 'MONTO TRASLADO OUT',";
-    $sql =$sql."1  AS 'SALDO FINAL'";
-    $sql =$sql."from nb_conosolidado_cartera_detalle_reporte a";
-    $sql =$sql.$sqlZonas;
+       ) AS 'FACTURAS VENDIDAS',
+       (
+            (
+            SELECT ifnull(sum(replace(replace(total,'$',''),',','')),0)  
+            from nb_ventas_grid_vw venta
+            JOIN nb_zonasFacturas_vw zonas ON (zonas.factura= venta.referencia)
+            where venta.estado = 'ACTIVO'
+            and  venta.formapago <> 'CONTADO'
+            AND zonas.nb_id_fld = reporte.ID_ZONA
+            AND date(fechaingreso) BETWEEN STR_TO_DATE('".$fecha_cartera_desde."','%d/%m/%Y') and STR_TO_DATE('".$fecha_cartera_hasta."','%d/%m/%Y')
+            )
+            -
+            (
+            SELECT ifnull(sum(replace(replace(venta.nb_abono_inicial_fld,'$',''),',','')),0) 
+            from nb_ventas_tbl venta
+            JOIN nb_zonasFacturas_vw zonas ON (zonas.factura= venta.nb_referencia_fld)
+            where venta.nb_estado_fld = 0
+            and  venta.nb_forma_pago_fld <> 0
+            AND zonas.nb_id_fld = reporte.ID_ZONA
+            AND (STR_TO_DATE(venta.nb_fecha_ingreso_fld, '%d/%m/%Y') BETWEEN STR_TO_DATE('".$fecha_cartera_desde."','%d/%m/%Y') and STR_TO_DATE('".$fecha_cartera_hasta."','%d/%m/%Y'))
+            
+            )
+       
+       )
+       AS 'NUEVA CARTERA',
+       (
+          select
+          count(a.nb_id_fld)
+          FROM nb_relacionfactura_tbl A
+          JOIN nb_zonasFacturas_vw zonas ON (zonas.factura= upper(A.NB_FACTURA_FLD))
+          WHERE zonas.nb_id_fld = reporte.ID_ZONA
+          AND (STR_TO_DATE(A.nb_fecha_retornado_fld, '%d/%m/%Y') BETWEEN STR_TO_DATE('".$fecha_cartera_desde."','%d/%m/%Y') and STR_TO_DATE('".$fecha_cartera_hasta."','%d/%m/%Y'))
+          AND A.nb_estado_fld = 0
+          AND A.nb_estado_cartera_fld = 3
+       
+       ) AS 'FACTURAS CASTIGADA',
+       (
+            (
+                select ifnull(sum(replace(replace(venta.saldo,'$',''),',','')),0)
+                from    nb_ventas_grid_vw venta
+                where   venta.referencia in 
+                (SELECT upper(A.NB_FACTURA_FLD)
+                    FROM nb_relacionfactura_tbl A
+                    JOIN nb_zonasFacturas_vw zonas ON (zonas.factura= upper(A.NB_FACTURA_FLD))
+                    WHERE zonas.nb_id_fld = reporte.ID_ZONA
+                      AND (STR_TO_DATE(A.nb_fecha_retornado_fld, '%d/%m/%Y') BETWEEN STR_TO_DATE('28/08/2017','%d/%m/%Y') AND STR_TO_DATE('09/09/2017','%d/%m/%Y'))
+                      AND A.nb_estado_fld = 0
+                      AND A.nb_estado_cartera_fld = 3
+                      AND NOT EXISTS  (
+                                            SELECT 'J'
+                                            FROM   nb_cartera_tbl cartera
+                                            WHERE SUBSTRING(upper(cartera.nb_referencia_fld),2,length(cartera.nb_referencia_fld)) = upper(A.NB_FACTURA_FLD)
+                                        )
+                )
+            )
+            
+        +
+        (
 
+             SELECT ifnull(sum(replace(replace(cartera.nb_nuevo_saldo_fld,'$',''),',','')),0)
+             FROM  nb_cartera_tbl cartera
+             where SUBSTRING(upper(cartera.nb_referencia_fld),2,length(cartera.nb_referencia_fld)) in
+             (
+                  select upper(A.NB_FACTURA_FLD)
+                  FROM nb_relacionfactura_tbl A
+                  JOIN nb_zonasFacturas_vw zonas ON (zonas.factura= upper(A.NB_FACTURA_FLD))
+                  WHERE zonas.nb_id_fld = reporte.ID_ZONA
+                  AND (STR_TO_DATE(A.nb_fecha_retornado_fld, '%d/%m/%Y') BETWEEN STR_TO_DATE('".$fecha_cartera_desde."','%d/%m/%Y') and STR_TO_DATE('".$fecha_cartera_hasta."','%d/%m/%Y'))
+                  AND A.nb_estado_fld = 0
+                  AND A.nb_estado_cartera_fld = 3
+
+             )
+             AND SUBSTRING(upper(cartera.nb_referencia_fld),1,1) ='V'
+             AND cartera.nb_id_fld = ( select max(cartera2.nb_id_fld) from nb_cartera_tbl cartera2 where cartera.nb_referencia_fld= cartera2.nb_referencia_fld )      
+
+           )
+      )
+       AS 'MONTO CASTIGADO'
+    FROM nb_conosolidado_cartera_detalle_reporte reporte ".$sqlZonas;    
+
+    
     $csv = new ExportExcel();
     $database->conectar();
     $file=$csv->exportarFile('0',$sql);
@@ -79,5 +213,5 @@
     header( 'Content-Length: '.filesize($file));
     header( 'Content-Disposition:attachment;filename='.$filename);
     readfile($file);
-    
+
 ?>
