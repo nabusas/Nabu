ca0142ab2b0a09b5e8fd9afdaed89f3da298f099
Carlos Alberto Garcia Cobo <carlosgc4@gmail.com>
2018-04-04 15:06:35 -0500

    4.44

--- Core/Class/Menu.php
+++ Core/Class/Menu.php
@@ -93,7 +93,7 @@ class Menu
                 if (
                         ($role == '2' and $enlace == 'nb_validacion_cartera_pg') or
                         ($role == '3' and $enlace == 'nb_cartera_v_pg') or
-                        ($role == '5' and $enlace == 'nb_cartera_v_pg') 
+                        ($role == '5' and $enlace == 'nb_productos_v_pg') 
                     )
                             echo '</ul>';
                 
